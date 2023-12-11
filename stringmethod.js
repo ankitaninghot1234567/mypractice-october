7e5da14c2bd0cf76e5fdf2128a1fd062dc63f2e8
ankitaninghot1234567 <ankita.awscloud@gmail.com>
2023-12-11 22:18:31 +0530

    fmkmflmf

--- stringmethod.js
+++ stringmethod.js
@@ -4,6 +4,33 @@ console.log(text.length);
 let text1='i live in pune';
 console.log(text1.slice(0,4));
 console.log(text1.slice(1));
-console.log(text1.slice(-6,-1));
-console.log(text1.substring(-5,8));
-console.log(text1.substr(0,4));
+console.log(text1.slice(-6,-1));//slice
+
+console.log(text1.substring(-5,8));//substring
+
+console.log(text1.substr(0,4))//subtr
+console.log(text1.substr(2,6));
+
+var datails = 'pune is big city is like pune' 
+console.log(datails.replace('pune','mumbai'));
+console.log(datails.replaceAll('pune',' thane'));
+console.log(datails.toUpperCase());
+console.log(datails.toLowerCase());
+
+var datails='ALDFEHF AHDIUAA UY8ASYA8O'
+console.log(datails.toLowerCase());
+
+let a= 'ankita'
+    b= 'ninghot' 
+   // let fullname= a.concat(' ',b)
+    console.log(a.concat('   ',b));
+
+    let text3 = '   hello world  '
+ console.log(text3.trim());
+ console.log(text3.trimStart());
+ console.log(text3.trimEnd());
+
+ myBday= '22/08/1995'
+ let year=myBday.split('/')
+ console.log(year);
+ console.log(year[2])
