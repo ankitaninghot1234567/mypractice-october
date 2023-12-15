7f67660cb85d49aad3a9b5d4717e070c978c8a8d
ankitaninghot1234567 <ankita.awscloud@gmail.com>
2023-12-15 19:48:23 +0530

    uhoijokopk

---
+++ condition.js
@@ -0,0 +1,22 @@
+marks=75
+//if
+if (marks>60){
+    console.log('distination');
+}
+
+//if else
+if (marks>80) {
+    console.log('pass..');
+    
+} else {
+    console.log('faied...');
+    
+}
+//esle if
+if (marks>85) {
+    console.log('first class');
+    
+} else if (marks>70 || marks<65) { 
+    console.log('sec class');
+}
+    
